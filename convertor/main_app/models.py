f331f710f98ecb63d94ff7279b9d11c7d7c6ecde
Chernov.Ilia <ilia.chernov@infotecs.ru>
2022-08-08 16:35:59 +0700

    правки

--- convertor/main_app/models.py
+++ convertor/main_app/models.py
@@ -2,15 +2,17 @@ from PIL import Image
 from django.db import models
 
 FORMAT_CHOICES = (("png", "png"),
-                  ("jpg", "jpg")
+                  ("jpeg", "jpeg")
 )
 
 
 class ImageModel(models.Model):
-    format = models.CharField(max_length=9, choices=FORMAT_CHOICES, default="jpg")
-    img = models.ImageField(upload_to="images/profile/",)
+    format = models.CharField(max_length=9, choices=FORMAT_CHOICES, default="jpeg")
+    img = models.ImageField(upload_to="images/before_convert/",)
 
     def convert(self):
         img = Image.open(self.img)
-        img.convert('RGB').save(f"image_name.{self.format}", self.format)
-        return img
+        print(f"LOG {self.format}  {self.img}  ")
+        rgb_img = img.convert('RGB')
+        rgb_img.save(f"static/{str(self.img).split('.')[0]}.{self.format}", self.format)
+        return rgb_img
