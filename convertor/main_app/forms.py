fe69d274f843e02d06ca3e27c25bba14763ae816
cidTomsk <61188943+cidTomsk@users.noreply.github.com>
2022-08-11 11:53:42 +0700

    добавление форматов и валидации

--- convertor/main_app/forms.py
+++ convertor/main_app/forms.py
@@ -5,4 +5,17 @@ from .models import ImageModel
 class ImageForm(forms.ModelForm):
     class Meta:
         model = ImageModel
-        fields = ("format", "img")
+        fields = ("img", "format")
+
+    def clean_format(self):
+        format = self.cleaned_data.get('format')
+        """ this will give you the actual username from the field
+            Now you may get that validated
+        """
+        img = self.cleaned_data.get('img')
+        if img is None:
+            return forms.ValidationError("Choose file before format")
+        elif format == str(img).split(".")[-1]:
+            return forms.ValidationError(f'Your choose same format {str(img).split(".")[-1]} ')
+        else:
+            return format
--- convertor/main_app/models.py
+++ convertor/main_app/models.py
@@ -2,13 +2,17 @@ from PIL import Image
 from django.db import models
 
 FORMAT_CHOICES = (("png", "png"),
-                  ("jpeg", "jpeg")
-)
+                  ("jpeg", "jpeg"),
+                  ("bmp", "bmp"),
+                  ("gif", "gif"),
+                  ("ico", "ico"),
+                  )
 
 
+# BMP, EPS, GIF, IM, JPEG, MSP, PCX PNG, PPM, TIFF, WebP, ICO
 class ImageModel(models.Model):
     format = models.CharField(max_length=9, choices=FORMAT_CHOICES, default="jpeg")
-    img = models.ImageField(upload_to="images/before_convert/",)
+    img = models.ImageField(upload_to="images/before_convert/", )
 
     def convert(self):
         img = Image.open(self.img)
