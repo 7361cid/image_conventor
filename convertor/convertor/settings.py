8f88c8d03f18293e269816202ee108174a37a342
ilia <chernoffilya1997@mail.ru>
2022-08-19 19:03:15 +0700

    env

--- convertor/convertor/settings.py
+++ convertor/convertor/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ags$#9q#5ld6of=^k^*n@=$4&lf$#ub%iffesqcnj008!l*9fq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', default=0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
